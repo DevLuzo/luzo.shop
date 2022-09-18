8e0a992f8516de7f2572cfca9c6c7db66f0f2952
bkaoluz <105419178+jammin5646@users.noreply.github.com>
2022-09-19 00:06:34 +0900

    cool

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-for(let i = 0; i < 100 ; i++){
+for(let i = 0; i < 30 ; i++){
     setTimeout(0.2)
     console.log(i)
+}
+
+function fun()
+{
+    console.log("exit");
+    window.open("ex.html");
 }
