85ec945abff076127ed0cdf5d2899456b019ceaf
bkaoluz <bkaoluz@gmail.com>
2022-11-25 20:37:48 +0900

    ff

--- script.js
+++ script.js
@@ -3,9 +3,3 @@ function sct()
     console.log("계산기")
     window.open("https://javascriptsat.netlify.com")
 }
-
-function home()
-{
-    console.log("home")
-    window.open("https://luzoshop-home.netlify.app/")
-}
